6eb3bdbb24967e4087fe82ba86327ebb0410e584
DB21 <jmds2115@gmail.com>
2018-08-25 14:22:56 +0530

    Added new Activities and implemenented Logical Parent Activity concept

--- Android1/app/src/main/java/com/alpha21/helloworldapp/MainActivity.java
+++ Android1/app/src/main/java/com/alpha21/helloworldapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.alpha21.helloworldapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendMessageBtn = findViewById(R.id.send_msg_btn);
+        sendMessageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage(v);
+            }
+        });
+    }
+
+    public void sendMessage(View view){
+        EditText messageEditText = findViewById(R.id.message);
+        String message = messageEditText.getText().toString();
+        Intent messageIntent = new Intent(this , MessageActivity.class);
+        messageIntent.putExtra("message", message);
+        startActivity(messageIntent);
     }
 }
